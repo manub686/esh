6b7e60add4e30818296effac3a21b11f052227bf
Manu Bansa <manub@stanford.edu>
2013-11-21 20:15:19 -0800

    separting out esh project from orsys

---
+++ conf.py
@@ -0,0 +1,10 @@
+[def]
+tcf = "/home/manub/workspace/orsys/targetconfs/evmbe6616_xds560v2_onechip_memconf1.ccxml"
+#prog = "home/manub/build-orsys/orilib_unittests/check_ORILIB_PacketSearch_Align.memconf1.none.out"
+prog = "/home/manub/workspace/build-orsys/app/wifirx54mc/app_wifirx54mc.memconf1.none.out"
+
+[debug]
+ds0 = "Blackhawk XDS560v2-USB System Trace Emulator_0/C66xx_00"
+ds1 = "Blackhawk XDS560v2-USB System Trace Emulator_0/C66xx_01"
+ds2 = "Blackhawk XDS560v2-USB System Trace Emulator_0/C66xx_02"
+ds3 = "Blackhawk XDS560v2-USB System Trace Emulator_0/C66xx_03"
