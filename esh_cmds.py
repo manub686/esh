6c461d19a066eed9869baa91ed00e53044518efc
Manu Bansa <manub@stanford.edu>
2013-11-24 02:15:51 -0800

    more improvements

--- esh_cmds.py
+++ esh_cmds.py
@@ -7,9 +7,13 @@ import traceback
 esh_jsutil = imp.load_source('esh_jsutil', os.environ['ESH_DIR'] + '/esh_jsutil.py')
 #from esh_jsutil import JSIOError
 
-LOW_INIT_LEVEL = 0
-DEBS_INIT_LEVEL = 4		#debug sessions have been created
-HIGH_INIT_LEVEL = 5		#targets have been connected
+LOW_INIT_LEVEL    = 0		#initial state, no init
+IMPORT_INIT_LEVEL = 1		#ccs java packages have been imported
+SCRIPT_INIT_LEVEL = 2		#scripting environment has been created
+CCS_INIT_LEVEL    = 3		#if wanted, ccs gui is started
+DEBSVR_INIT_LEVEL = 4		#debug server has been created
+DEBSES_INIT_LEVEL = 5		#debug sessions have been created
+HIGH_INIT_LEVEL   = 6		#targets have been connected
 
 def cmd_help(jsproc, options, args):
   selfmodule = sys.modules[__name__]
@@ -18,15 +22,28 @@ def cmd_help(jsproc, options, args):
 
   return 0
 
+def cmd_ping(jsproc, options, args):
+  print 'ping request received'
+  print 'returning 0'
+
+  return 0
+
 def cmd_bye(jsproc, options, args):
   pass
+
+  return 0
+
+def cmd_options(jsproc, options, args):
+  print options
 
   return 0
 
 def cmd_prog(jsproc, options, args):
   if len(args) > 0:
     prog = args[0]
+    prog = ensure_quoted(prog)
     print 'Setting program to', prog
+    options.prog = prog
   else:
     prog = options.prog
 
@@ -60,8 +77,27 @@ def cmd_load(jsproc, options, args):
   print 'loading program to', session_id
   print 'program image:', prog
 
-  esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_load_program(session_id, prog))
-  return 0
+  r = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_load_program(session_id, prog))
+  return r
+
+def ensure_quoted(s):
+  if (not s.startswith('"')) and (not s.endswith('"')):
+    return '"' + s + '"'
+  else:
+    return s
+  
+def cmd_loadall(jsproc, options, args):
+  if not assert_init_level(HIGH_INIT_LEVEL, options):
+    return -1
+
+  rs = []
+  for session_id in options.debugSessions:
+    print 'loading on debug session', session_id
+    r = cmd_load(jsproc, options, [session_id])
+    print 'retval=%d' % r
+    rs.append(r)
+  r = sum([abs(x) for x in rs])
+  return r
 
 def cmd_run(jsproc, options, args):
   if not assert_init_level(HIGH_INIT_LEVEL, options):
@@ -73,8 +109,21 @@ def cmd_run(jsproc, options, args):
     print 'ERROR: need a session_id to identify run target'
     return 1
 
-  esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_run_target(session_id))
-  return 0
+  r = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_run_target(session_id))
+  return r
+
+def cmd_runall(jsproc, options, args):
+  if not assert_init_level(HIGH_INIT_LEVEL, options):
+    return -1
+
+  rs = []
+  for session_id in options.debugSessions:
+    print 'running debug session', session_id
+    r = cmd_run(jsproc, options, [session_id])
+    print 'retval=%d' % r
+    rs.append(r)
+  r = sum([abs(x) for x in rs])
+  return r
 
 def cmd_halt(jsproc, options, args):
   if not assert_init_level(HIGH_INIT_LEVEL, options):
@@ -89,6 +138,19 @@ def cmd_halt(jsproc, options, args):
   esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_halt_target(session_id))
   return 0
 
+def cmd_haltall(jsproc, options, args):
+  if not assert_init_level(HIGH_INIT_LEVEL, options):
+    return -1
+
+  rs = []
+  for session_id in options.debugSessions:
+    print 'halting debug session', session_id
+    r = cmd_halt(jsproc, options, [session_id])
+    print 'retval=%d' % r
+    rs.append(r)
+  r = sum([abs(x) for x in rs])
+  return r
+
 def cmd_restart(jsproc, options, args):
   if not assert_init_level(HIGH_INIT_LEVEL, options):
     return -1
@@ -102,8 +164,21 @@ def cmd_restart(jsproc, options, args):
   esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_restart_target(session_id))
   return 0
 
+def cmd_restartall(jsproc, options, args):
+  if not assert_init_level(HIGH_INIT_LEVEL, options):
+    return -1
+
+  rs = []
+  for session_id in options.debugSessions:
+    print 'restarting debug session', session_id
+    r = cmd_restart(jsproc, options, [session_id])
+    print 'retval=%d' % r
+    rs.append(r)
+  r = sum([abs(x) for x in rs])
+  return r
+
 def cmd_reset(jsproc, options, args):
-  if not assert_init_level(DEBS_INIT_LEVEL, options):
+  if not assert_init_level(DEBSES_INIT_LEVEL, options):
     return -1
 
   if len(args) > 0:
@@ -114,6 +189,27 @@ def cmd_reset(jsproc, options, args):
 
   esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_reset_target(session_id))
   return 0
+
+def cmd_startccs(jsproc, options, args):
+  if not assert_init_level(SCRIPT_INIT_LEVEL, options):
+    return -1
+
+  print 'Starting CCS'
+  r = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_start_ccs())
+  print
+  #wait = 20
+  #print 'Waiting %d secs to let CCS start...' % wait
+  #time.sleep(wait)
+
+  return r
+
+def cmd_stopccs(jsproc, options, args):
+  print 'Stopping CCS'
+  r = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_stop_ccs())
+  time.sleep(10)
+
+  return r
+
 
 def cmd_evminitlevel(jsproc, options, args):
   print 'EVM initlevel = %d' % options.init
@@ -161,25 +257,27 @@ def evminit2(jsproc, options, args):
   print 'evminit2: Creating scripting environment'
   r1 = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_create_scripting_env())
 
-  if ((not options.cleanup) and options.gui):
-    print 'Starting CCS'
-    esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_start_ccs())
-    print 'Waiting 5 secs to let CCS start...'
-    time.sleep(5)
-
   return r1
 
-#init3
 def evminit3(jsproc, options, args):
-  print 'evminit3: Starting debug server'
+  r = 0
+  if ((not options.cleanup) and options.gui):
+    print 'evminit3: Starting ccs'
+    r = cmd_startccs(jsproc, options, args)
+
+  return r
+
+#init4
+def evminit4(jsproc, options, args):
+  print 'evminit4: Starting debug server'
   r2 = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_start_debugger(options.tcf))
   time.sleep(10)
 
   return r2
 
-#init4
-def evminit4(jsproc, options, args):
-  print 'evminit4: Starting debug sessions'
+#init5
+def evminit5(jsproc, options, args):
+  print 'evminit5: Starting debug sessions'
   r3 = []
   for session_id in options.debugSessions:
     target_name = options.debugSessions[session_id]
@@ -189,9 +287,9 @@ def evminit4(jsproc, options, args):
 
   return sum([abs(r) for r in r3])
     
-#init5
-def evminit5(jsproc, options, args):
-  print 'evminit5: Connecting debug sessions'
+#init6
+def evminit6(jsproc, options, args):
+  print 'evminit6: Connecting debug sessions'
   r4 = []
   for session_id in options.debugSessions:
     r = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_connect_debug_session(session_id))
@@ -245,12 +343,12 @@ def cmd_evmshutdown(jsproc, options, args):
 
   return 0
 
-def evmshutdown5(jsproc, options, args):
+def evmshutdown6(jsproc, options, args):
   print 'NOP'
   
   return 0
 
-def evmshutdown4(jsproc, options, args):
+def evmshutdown5(jsproc, options, args):
   print 'Stopping debug sessions'
   r3 = []
   for session_id in options.debugSessions:
@@ -258,20 +356,23 @@ def evmshutdown4(jsproc, options, args):
     r3.append(r)
     time.sleep(4)
 
-
-  if ((not options.cleanup) and options.gui):
-    print 'Stopping CCS'
-    esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_stop_ccs())
-    time.sleep(10)
-
   return sum([abs(r) for r in r3])
 
-def evmshutdown3(jsproc, options, args):
+
+def evmshutdown4(jsproc, options, args):
   print 'Stopping debug server'
   r2 = esh_jsutil.exec_jscmd(jsproc, esh_jsutil.code_stop_debug_server())
   time.sleep(5)
 
   return r2
+
+def evmshutdown3(jsproc, options, args):
+  r = 0
+  if ((not options.cleanup) and options.gui):
+    print 'Stopping ccs'
+    r = cmd_stopccs(jsproc, options, args)
+
+  return r
 
 def evmshutdown2(jsproc, options, args):
   print 'Stopping scripting environment'
